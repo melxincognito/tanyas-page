978b868b18e383f81933c9c97a1fd75bc513ec71
Mel Incognito <melxincognito@gmail.com>
2022-02-28 18:15:29 -0700

    added framer motion

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Box, Card, CardContent, Typography } from "@mui/material";
 import FrontCover from "../assets/housePool_2.jpeg";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default class HomePage extends Component {
   render() {
@@ -9,13 +10,17 @@ export default class HomePage extends Component {
       alignContent: "center",
       justifyContent: "center",
     };
-    const topImageStyles = {
+    const imageContainerStyles = {
       display: "flex",
       alignContent: "center",
       justifyContent: "center",
       alignItems: "center",
       justifyItems: "center",
       width: "100%",
+    };
+    const imageStyle = {
+      width: "100%",
+      height: "90%",
     };
     const cardStyles = {
       borderRadius: "5px 25px 5px 25px",
@@ -29,41 +34,45 @@ export default class HomePage extends Component {
       padding: 2,
       spacing: 12,
     };
+
     return (
-      <>
-        <Box sx={mainContainerStyles}>
-          <Box sx={topImageStyles}>
-            <img
-              style={{
-                width: "100%",
-                height: "90%",
-                borderRadius: 5,
-              }}
-              src={FrontCover}
-              alt="
+      <AnimatePresence>
+        <motion.div
+          transition={{ delay: 0.17 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <Box sx={mainContainerStyles}>
+            <Box sx={imageContainerStyles}>
+              <img
+                style={imageStyle}
+                src={FrontCover}
+                alt="
         pool house"
-            />
-            <Box sx={{ position: "absolute" }}>
-              <Card sx={cardStyles}>
-                <CardContent>
-                  <div>
-                    <Typography variant="h4"> TanyA Gonzalez</Typography>
-                  </div>
-                  <div>
-                    <Typography variant="h4" color="secondary.main">
-                      Az Realtor
-                    </Typography>
-                  </div>{" "}
-                  <div>
-                    <Typography variant="h4"> I am amazing</Typography>
-                  </div>
-                </CardContent>
-              </Card>
+              />
+              <Box sx={{ position: "absolute" }}>
+                <Card sx={cardStyles}>
+                  <CardContent>
+                    <div>
+                      <Typography variant="h4"> TanyA Gonzalez</Typography>
+                    </div>
+                    <div>
+                      <Typography variant="h4" color="secondary.main">
+                        Az Realtor
+                      </Typography>
+                    </div>{" "}
+                    <div>
+                      <Typography variant="h4"> I am amazing</Typography>
+                    </div>
+                  </CardContent>
+                </Card>
+              </Box>
             </Box>
+            <div> it's me</div>
           </Box>
-          <div> it's me</div>
-        </Box>
-      </>
+        </motion.div>
+      </AnimatePresence>
     );
   }
 }
